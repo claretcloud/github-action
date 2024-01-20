0e8e4d133f7c317f50b7f1cc94f168621002456d
Young Maryclaret Nnenna <claretyoung@gmail.com>
2024-01-20 18:57:27 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import os
+
+# Get a specific environment variable
+api_key = os.environ.get ("'API_KEY")
+
+# Print the value or handle if it's not set
+if api_key:
+  print(f"API Key: {api_key}")
+else:
+
+print("API Key not set.")
